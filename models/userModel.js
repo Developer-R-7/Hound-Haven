6d30839b161aab744d09995c6b615283a94be320
Shellsea <elainesbarrera@gmail.com>
2021-03-13 12:58:30 -0800

    ADDED: confirmed value for userModel set to false initially

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		match: [/.+@.+\..+/, "Enter a valid email"],
-	},
-	password: { type: String, required: true, minLength: 5 },
-	displayName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Enter a valid email"],
+  },
+  password: { type: String, required: true, minLength: 5 },
+  displayName: { type: String, required: true },
+  confirmed: { type: Boolean, default: false },
 });
 
 module.exports = User = mongoose.model("user", userSchema);
