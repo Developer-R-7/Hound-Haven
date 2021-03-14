ebb8460ea0245dcdf285be1de686f16b703a1163
Kailee Smith <kaileesmith1501@gmail.com>
2021-03-13 22:04:45 -0800

    added start to add meds modal as test

--- client/src/Components/Modals/AddMeds.js
+++ client/src/Components/Modals/AddMeds.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const AddMeds = () => {
-	return <div></div>;
+	return <div className="modal" id="addAPetModal" tabIndex="-1">
+				<div className="modal-dialog modal-lg">
+					<div className="modal-content">
+						<div className="modal-header">
+							<button
+								type="button"
+								className="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+							></button>
+						</div>
+						<div
+							style={{ display: "inline-flex", justifyContent: "center" }}
+							className="modal-body"
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+
 };
 
 export default AddMeds;
