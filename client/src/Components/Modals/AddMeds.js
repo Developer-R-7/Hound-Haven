d5f9536a784971e22ba6dc27de662bf7e35c78c0
Kailee Smith <kaileesmith1501@gmail.com>
2021-03-13 22:07:46 -0800

    testing push with modal

--- client/src/Components/Modals/AddMeds.js
+++ client/src/Components/Modals/AddMeds.js
@@ -1,25 +1,7 @@
 import React from "react";
 
 const AddMeds = () => {
-	return <div className="modal" id="addAPetModal" tabIndex="-1">
-				<div className="modal-dialog modal-lg">
-					<div className="modal-content">
-						<div className="modal-header">
-							<button
-								type="button"
-								className="btn-close"
-								data-bs-dismiss="modal"
-								aria-label="Close"
-							></button>
-						</div>
-						<div
-							style={{ display: "inline-flex", justifyContent: "center" }}
-							className="modal-body"
-						>
-					</div>
-				</div>
-			</div>
-		</div>
+	return <div>Hello World</div>
 
 };
 
