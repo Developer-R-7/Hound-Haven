6baa9993a0fbf6762a33c65cdd12a51b89847155
Shellsea <elainesbarrera@gmail.com>
2021-03-14 15:05:52 -0700

    ADDED: super basic layout to petDash

--- client/src/Pages/PetDash.js
+++ client/src/Pages/PetDash.js
@@ -7,13 +7,79 @@ const PetDash = () => {
   const { userData } = useContext(UserContext);
   const history = useHistory();
 
-  useEffect(() => {
-    if (!userData.user) history.push("/");
-  }, [userData.user, history]);
+  // useEffect(() => {
+  //   if (!userData.user) history.push("/login");
+  // }, [userData.user, history]);
 
+  const buttonStyle = {
+    backgroundColor: "rgb(255, 100, 100)",
+  };
   return (
-    <div>
-      <h1>PetDash</h1>
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-3">
+            <div className="card">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081"
+                className="card-img-top"
+                alt="petImage"
+              ></img>
+              <div className="card-body">
+                <h1 className="card-title">Pet Info</h1>
+                <h4 className="card-title">Name</h4>
+                <h4 className="card-title">Birthday</h4>
+                <h4 className="card-title">Gender</h4>
+                <h4 className="card-title">Breed</h4>
+
+                <button style={buttonStyle} className="btn">
+                  Edit
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-9">
+            <div className="row">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Button
+                  </a>
+                </div>
+              </div>{" "}
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Button
+                  </a>
+                </div>
+              </div>{" "}
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </p>
+                  <a href="#" class="btn btn-primary">
+                    Button
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
