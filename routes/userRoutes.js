d4a0176178c29582879c36cecba55b8c425ce1fe
Sam Ayler <sayler8701@gmail.com>
2021-03-19 14:56:55 -0700

    ADD: cascade delete for user and pets backend working

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
-const { register, login, getUser } = require("../controllers/UserController");
+const {
+	register,
+	login,
+	getUser,
+	deleteUser,
+} = require("../controllers/UserController");
 
 router.post("/register", register);
 
 router.post("/login", login);
 
 router.get("/", auth, getUser);
+
+router.delete("/delete/:id", deleteUser);
 
 module.exports = router;
