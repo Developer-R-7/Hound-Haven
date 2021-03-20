bf5298311facc235550ab04ace4db4e59de9a556
Sam Ayler <sayler8701@gmail.com>
2021-03-20 09:04:46 -0700

    ADD: adding bell to navbar for notifications

--- client/src/Components/Modals/Notify.js
+++ client/src/Components/Modals/Notify.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React from "react";
 
 const Notify = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div className="modal" id="notifyModal" tabindex="-1">
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+						{/* <h5 class="modal-title">Modal title</h5> */}
+						<button
+							type="button"
+							className="btn-close"
+							data-bs-dismiss="modal"
+							aria-label="Close"
+						></button>
+					</div>
+					<div className="modal-body">
+						<div>
+							<ul className="nav nav-tabs" id="myTab" role="tablist">
+								<li className="nav-item" role="presentation">
+									<button
+										className="nav-link active"
+										id="home-tab"
+										data-bs-toggle="tab"
+										data-bs-target="#home"
+										type="button"
+										role="tab"
+										aria-controls="home"
+										aria-selected="true"
+									>
+										Pet 1
+									</button>
+								</li>
+								<li className="nav-item" role="presentation">
+									<button
+										className="nav-link"
+										id="profile-tab"
+										data-bs-toggle="tab"
+										data-bs-target="#profile"
+										type="button"
+										role="tab"
+										aria-controls="profile"
+										aria-selected="false"
+									>
+										Pet 2
+									</button>
+								</li>
+								<li className="nav-item" role="presentation">
+									<button
+										className="nav-link"
+										id="contact-tab"
+										data-bs-toggle="tab"
+										data-bs-target="#contact"
+										type="button"
+										role="tab"
+										aria-controls="contact"
+										aria-selected="false"
+									>
+										Pet 3
+									</button>
+								</li>
+							</ul>
+							<div className="tab-content" id="myTabContent">
+								<div
+									className="tab-pane fade show active"
+									id="home"
+									role="tabpanel"
+									aria-labelledby="home-tab"
+								>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Similique, fugiat.
+								</div>
+								<div
+									className="tab-pane fade"
+									id="profile"
+									role="tabpanel"
+									aria-labelledby="profile-tab"
+								>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
+									dolorem.
+								</div>
+								<div
+									className="tab-pane fade"
+									id="contact"
+									role="tabpanel"
+									aria-labelledby="contact-tab"
+								>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Fugiat, explicabo.
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="modal-footer">
+						{/* <button type="button" class="btn btn-primary">
+							Save changes
+						</button> */}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Notify
+export default Notify;
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,115 +1,91 @@
 import React, { Fragment, useEffect, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../Context/UserContext";
+import Notify from "./Modals/Notify";
 
 const NavBar = () => {
-  const { userData, setUserData } = useContext(UserContext);
-  const [links, setLinks] = useState(null);
+	const { userData, setUserData } = useContext(UserContext);
+	const [links, setLinks] = useState(null);
 
-  const logout = () => {
-    setUserData({ token: undefined, user: undefined });
-    localStorage.setItem("auth-token", "");
-  };
+	const logout = () => {
+		setUserData({ token: undefined, user: undefined });
+		localStorage.setItem("auth-token", "");
+	};
 
-  const linkStyle = {
-    textDecoration: "none",
-    color: "black",
-    margin: "25px",
-  };
+	const linkStyle = {
+		textDecoration: "none",
+		color: "black",
+		margin: "25px",
+	};
 
-  useEffect(() => {
-    if (!userData.user) {
-      setLinks(
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <Link to="/login" style={linkStyle}>
-              Login
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/register" style={linkStyle}>
-              Register
-            </Link>
-          </li>
-        </ul>
-        // <Fragment>
-        //   <li>
-        //     <Link to="/login">Login</Link>
-        //   </li>
-        //   <li>
-        //     <Link to="/register">Register</Link>
-        //   </li>
-        // </Fragment>
-      );
-    } else {
-      setLinks(
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <Link to="/notify"></Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/" style={linkStyle} onClick={logout}>
-              Logout
-            </Link>
-          </li>
-          <li class="nav-item">
-            <Link to="/home" style={linkStyle}>
-              Home
-            </Link>
-          </li>
-        </ul>
-      );
+	useEffect(() => {
+		if (!userData.user) {
+			setLinks(
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link to="/login" style={linkStyle}>
+							Login
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/register" style={linkStyle}>
+							Register
+						</Link>
+					</li>
+				</ul>
+			);
+		} else {
+			setLinks(
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link data-bs-toggle="modal" data-bs-target="#notifyModal">
+							<i className="bi bi-bell"></i>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" style={linkStyle} onClick={logout}>
+							Logout
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/home" style={linkStyle}>
+							Home
+						</Link>
+					</li>
+				</ul>
+			);
+		}
+	}, [userData]);
 
-      // <Fragment>
-      //   <li className="nav-item active">
-      //     <Link to="/notify"></Link>
-      //   </li>
-      //   <li className="nav-item active">
-      //     <Link to="/" onClick={logout}>
-      //       Logout
-      //     </Link>
-      //   </li>
-      //   <li className="nav-item active">
-      //     <Link to="/home">Home</Link>
-      //   </li>
-      // </Fragment>
-    }
-  }, [userData]);
-
-  return (
-    <nav
-      class="navbar navbar-expand-lg navbar-light"
-      style={{ backgroundColor: "#FFE2E2", height: "60px" }}
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          MyPet
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          {links}
-        </div>
-      </div>
-    </nav>
-    // <div>
-    // 	<nav className="navbar navbar-light">
-    // 			<a>My Pet</a>
-    // 			<ul className="navbar-nav mr-auto">
-    // 			{links}
-    // 			</ul>
-    // 	</nav>
-    // </div>
-  );
+	return (
+		<>
+			<nav
+				className="navbar navbar-expand-lg navbar-light"
+				style={{ backgroundColor: "#FFE2E2", height: "60px" }}
+			>
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						MyPet
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNavDropdown">
+						{links}
+					</div>
+				</div>
+			</nav>
+			<Notify />
+		</>
+	);
 };
 
 export default NavBar;
