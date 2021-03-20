1a5064e3dd588733ba51cf23c1d1f5c6bc64f3cf
Sam Ayler <sayler8701@gmail.com>
2021-03-19 18:49:16 -0700

    ADD: delete user functionality working

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,35 +1,51 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import UserContext from "../Context/UserContext";
 
-const Footer = () => {
-  const style = {
-    backgroundColor: "#ffc2c2",
-    borderTop: "1px solid #E7E7E7",
-    textAlign: "center",
-    padding: "20px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    height: "60px",
-    width: "100%",
-  };
+const Footer = (props) => {
+	const { userData, setUserData } = useContext(UserContext);
+	const style = {
+		backgroundColor: "#ffc2c2",
+		borderTop: "1px solid #E7E7E7",
+		textAlign: "center",
+		padding: "20px",
+		position: "fixed",
+		left: "0",
+		bottom: "0",
+		height: "60px",
+		width: "100%",
+	};
 
-  const invisible = {
-    display: "block",
-    padding: "20px",
-    height: "60px",
-    width: "100%",
-  };
+	const invisible = {
+		display: "block",
+		padding: "20px",
+		height: "60px",
+		width: "100%",
+	};
 
-  return (
-    <div>
-      <div style={invisible}>
-        <div style={style}>
-          <span>From pet parents to you</span>
-          <button>delete account</button>
-        </div>
-      </div>
-    </div>
-  );
+	const handleDelete = async (e) => {
+		e.preventDefault();
+		try {
+			await axios.delete(`/users/delete/${userData.user.id}`, {
+				headers: { "x-auth-token": localStorage.getItem("auth-token") },
+			});
+			setUserData({ token: undefined, user: undefined });
+			localStorage.setItem("auth-token", "");
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<div>
+			<div style={invisible}>
+				<div style={style}>
+					<span>From pet parents to you</span>
+					<button onClick={handleDelete}>delete account</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
