36368fa12f6cdaf3f43b47cfeb554ce25722d4c5
faitheden1 <faitheden54@gmail.com>
2021-03-23 17:27:44 -0700

    merging card

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -1,18 +1,7 @@
 import React from "react";
 
-const Card = function (props)  {
-  return <div>
-
-<div className="card text-center w-75">
-            <h5 className="card-title"> {props.title}</h5>	
-                <div className="card-body">
-                    <img src={props.imageUrl} 
-                    className="p-3 img-responsive rounded-circle w-10"/>
-                    <h6 className="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
-                    <p className="card-text">{props.body} </p>
-                </div>
-            </div>
-  </div>;
+const Card = () => {
+  return <div>About our app goes here!</div>;
 };
 
 export default Card;
