97928d4ccd0b23f1c9031fd1a466582ab7a0ded6
Shellsea <elainesbarrera@gmail.com>
2021-03-24 14:45:42 -0700

    FIX: text in about components to match pitch of presentation

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -2,27 +2,30 @@ import React from "react";
 import logo from "./paw_logo.PNG";
 
 const About = () => {
-    
-    return (
-        <div className="col-md-5  m-4">
-            <div className="card text-center w-75">
-                <h5 className="card-title">The My Pet App</h5>
-                <div className="card-body">
-                    <img
-                        src={logo}
-                        alt="petLogo"
-                        className="p-3 img-fluid rounded-circle w-10"
-                    ></img>
+  return (
+    <div className="col-md-5  m-4">
+      <div className="card text-center w-75">
+        <h5 className="card-title">The My Pet App</h5>
+        <div className="card-body">
+          <img
+            src={logo}
+            alt="petLogo"
+            className="p-3 img-fluid rounded-circle w-10"
+          ></img>
 
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">
-                        Application to track your pet's health and help you track when it's
-            time for vaccinations and give meds.{" "}
-                    </p>
-                </div>
-            </div>
+          <p className="card-text">
+            Love your pet and want to make sure they are well taken care of? It
+            can be difficult to keep up with regular pet exams, yearly
+            vaccinations, and medications. After all, we have a hard enough time
+            keeping up with our own health let alone that of our furry,
+            feathery, or scaly friends. That's where My Pet comes in! A user
+            friendly app that keeps track of your pets and all their medical
+            needs for a healthier life.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -137,8 +137,14 @@ const NavBar = () => {
                     ></img>
 
                     <p className="card-text">
-                      Application to track your pet's health and help you track
-                      when it's time for vaccinations and give meds.{" "}
+                      Love your pet and want to make sure they are well taken
+                      care of? It can be difficult to keep up with regular pet
+                      exams, yearly vaccinations, and medications. After all, we
+                      have a hard enough time keeping up with our own health let
+                      alone that of our furry, feathery, or scaly friends.
+                      That's where My Pet comes in! A user friendly app that
+                      keeps track of your pets and all their medical needs for a
+                      healthier life.
                     </p>
                   </div>
                 </div>
