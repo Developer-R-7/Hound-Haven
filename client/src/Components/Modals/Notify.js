4dca117a35b0fc2751a988c4fcf0165e98a74e6a
Kathleen Welch <ktywelch@gmail.com>
2021-03-23 18:39:23 -0700

    notify

--- client/src/Components/Modals/Notify.js
+++ client/src/Components/Modals/Notify.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useContext} from 'react'
+import React, {useState,useEffect,useReducer} from 'react'
 import Moment from 'react-moment'
 import {Tabs, Tab, Button } from "react-bootstrap";
 
@@ -7,26 +7,14 @@ import {Tabs, Tab, Button } from "react-bootstrap";
 
 const Notify = (props) => {
     const [notifyItems, setNotifyItems] = useState(props?.vals)
-	const [key, setKey] = useState('Medications');
-  
-	let petmeds = {};
-	let vetvisits = {};
-	let reminders = {};
+	const [key, setKey] = useState('meds');
+	const [med, setMed] = useState(notifyItems?notifyItems[0]:null);
+	const [vet, setVet] = useState(notifyItems?notifyItems[1]:null);
+	const [rem, setRem] = useState(notifyItems?notifyItems[2]:null);
 
+	console.log(med);
 		useEffect(() => {
-			if (notifyItems[0]) {
-				petmeds = notifyItems[0];
-				console.log(petmeds)}
-			if (notifyItems[1]) vetvisits = notifyItems[1];
-			if (notifyItems[2]) reminders = notifyItems[2];
-        console.log(vetvisits)
-			//for(let i = 0; i < petmeds.length; i++) console.log(petmeds[i])
-			vetvisits.forEach(pet => {
-				console.log('here',pet)
-			});
-			// return () => {
-		
-			// }
+			console.log(med);
 		}, [notifyItems])
 	
 
@@ -35,123 +23,58 @@ const Notify = (props) => {
 
 
 		<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-		<Tab eventKey="home" title="Home">
-		<div>
-				Here	 					{ petmeds && (
-							            <li>
-					 						{petmeds.Pet}
-					 						<Moment utc format="MM/DD/YYYY">
-					 							{petmeds.Date}
-					 						</Moment>	
-					 						{petmeds.Medication}
-					 						{petmeds.Dose}
-					 			         </li>
-					 					)}
+		<Tab eventKey="meds" title="Medications">
+			<ul>
+				
+					{ med.map((appt) =>( 
+					
+				<li >
+					
+					{appt.Pet} &nbsp; 
+					<Moment utc format="MM/DD/YYYY">
+						{appt.Date}
+					</Moment>	
+					&nbsp; {appt.Medication}&nbsp; 
+					{appt.Dose}
+					</li>
+				))}
 
-</div>
-										 Test
-					 				
+			</ul>
+				
+			
 		</Tab>
-		<Tab eventKey="profile" title="Profile">
-		
+		<Tab eventKey="vets" title="Vet Visits">
+		<ul>
+		  { vet.map((appt1) =>( 	
+			<li >
+				{appt1.Pet} &nbsp; 
+				<Moment utc format="MM/DD/YYYY">
+					{appt1.Date}
+				</Moment>	
+				&nbsp;{appt1.Notes}
+				</li>
+			))}
+
+		</ul>
 		</Tab>
-		<Tab eventKey="contact" title="Contact" disabled>
+		<Tab eventKey="rems" title="Reminders" disabled>
+		{ rem.map((appt2) =>( 	
+			<li >
+				{appt2.Pet} &nbsp; 
+				<Moment utc format="MM/DD/YYYY">
+					{appt2.Date}
+				</Moment>	
+				&nbsp; {appt2.Title}&nbsp; 
+				{appt2.Notes}
+				</li>
+			))}
+
 		 
 		</Tab>
 	  </Tabs>
 
 
-
-
-					// 	<div>
-					// 		<ul className="nav nav-tabs" id="myTab" role="tablist">
-					// 			<li className="nav-item" role="presentation">
-					// 				<button
-					// 					className="nav-link active"
-					// 					id="home-tab"
-					// 					data-bs-toggle="tab"
-					// 					data-bs-target="#home"
-					// 					type="button"
-					// 					role="tab"
-					// 					aria-controls="home"
-					// 					aria-selected="true"
-					// 				>
-					// 					Medications
-					// 				</button>
-					// 			</li>
-					// 			<li className="nav-item" role="presentation">
-					// 				<button
-					// 					className="nav-link"
-					// 					id="profile-tab"
-					// 					data-bs-toggle="tab"
-					// 					data-bs-target="#profile"
-					// 					type="button"
-					// 					role="tab"
-					// 					aria-controls="profile"
-					// 					aria-selected="false"
-					// 				>
-					// 					Visits
-					// 				</button>
-					// 			</li>
-					// 			<li className="nav-item" role="presentation">
-					// 				<button
-					// 					className="nav-link"
-					// 					id="contact-tab"
-					// 					data-bs-toggle="tab"
-					// 					data-bs-target="#contact"
-					// 					type="button"
-					// 					role="tab"
-					// 					aria-controls="contact"
-					// 					aria-selected="false"
-					// 				>
-					// 					Reminders
-					// 				</button>
-					// 			</li>
-					// 		</ul>
-					// 		<div className="tab-content" id="myTabContent">
-					// 			<div
-					// 				className="tab-pane fade show active"
-					// 				id="home"
-					// 				role="tabpanel"
-					// 				aria-labelledby="home-tab"
-					// 			>
-			 		// 		      <ul>
-					// 					{ Object.keys(petmeds).map((med,i) => (
-					// 					<li	
-					// 					    key={i}
-					// 						className="pet-list card-body">
-					// 						{petmeds[med].Pet}
-					// 						<Moment utc format="MM/DD/YYYY">
-					// 							{petmeds[med].Date}
-					// 						</Moment>	
-					// 						{petmeds[med].Medication}
-					// 						{petmeds[med].Dose}
-					// 					</li>
-					// 					))}
-					// 				</ul>
-					// 			<div
-					// 				className="tab-pane fade"
-					// 				id="profile"
-					// 				role="tabpanel"
-					// 				aria-labelledby="profile-tab"
-					// 			>
-					// 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
-					// 				dolorem.
-					// 			</div>
-					// 			<div
-					// 				className="tab-pane fade"
-					// 				id="contact"
-					// 				role="tabpanel"
-					// 				aria-labelledby="contact-tab"
-					// 			>
-					// 				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					// 				Fugiat, explicabo.
-					// 			</div>
-					// 		</div>
-					// 	</div>
-					// </div>
-					
 	);
-};
+	};
 
 export default Notify;
